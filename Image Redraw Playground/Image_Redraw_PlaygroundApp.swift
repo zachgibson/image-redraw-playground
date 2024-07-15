4eefbb468fd3607aed32a6c5c2424aceacc13255
Zachary Gibson <zackgib@aol.com>
2024-07-15 08:36:40 -0500

    Initial Commit

---
+++ Image Redraw Playground/Image_Redraw_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_Redraw_PlaygroundApp.swift
+//  Image Redraw Playground
+//
+//  Created by Zachary Gibson on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Image_Redraw_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
