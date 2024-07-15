101e87e7a25fe5d262950acd75deb7c1f5364e45
Zachary Gibson <zackgib@aol.com>
2024-07-15 09:43:13 -0500

    Initial commit

--- Image Redraw Playground/ContentView.swift
+++ Image Redraw Playground/ContentView.swift
@@ -7,15 +7,69 @@
 
 import SwiftUI
 
+enum ImageAction: String {
+    case writeMonochromaticImage
+    case writePolychromaticImage
+    case removeImage
+}
+
 struct ContentView: View {
+    let imageURL = URL.documentsDirectory.appending(component: "spidey.png")
+    @State var imageAction: ImageAction? = nil
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let imageAction = imageAction {
+                Text(imageAction.rawValue)
+            }
+            AsyncImage(url: imageURL) { imagePhase in
+                switch imagePhase {
+                case let .success(image):
+                    image
+                        .resizable()
+                        .scaledToFit()
+                case .empty:
+                    Color.red
+                        .frame(maxHeight: 300)
+                case .failure:
+                    Color.red
+                        .frame(maxHeight: 300)
+                }
+            }
+            Button("Remove Image Data") {
+                imageAction = .removeImage
+            }
+            Button("Update Image Data") {
+                if imageAction == .writePolychromaticImage {
+                    imageAction = .writeMonochromaticImage
+                } else {
+                    imageAction = .writePolychromaticImage
+                }
+            }
+            .onChange(of: imageAction) { oldValue, newValue in
+                switch newValue {
+                case .writeMonochromaticImage:
+                    if let imageData = UIImage(resource: .monoChromaticSpidey).pngData() {
+                        writeImageToDisk(data: imageData)
+                    }
+                case .writePolychromaticImage:
+                    if let imageData = UIImage(resource: .polyChromaticSpidey).pngData() {
+                        writeImageToDisk(data: imageData)
+                    }
+                case .removeImage:
+                    removeImageFromDisk()
+                case .none:
+                    break
+                }
+            }
         }
         .padding()
+    }
+    func writeImageToDisk(data: Data) {
+        FileManager.default.createFile(atPath: imageURL.path(), contents: data)
+    }
+    func removeImageFromDisk() {
+        try! FileManager.default.removeItem(at: imageURL)
     }
 }
 
